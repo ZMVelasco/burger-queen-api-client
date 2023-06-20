f7dff46185cc6f2f3a63501bcdf0838eec4d0fb4
“Mariana <zaira.vl21@gmail.com>
2023-06-20 15:35:26 -0600

    Creates getProducts function that brings all existing products and creates a card for each

--- burger-queen-api-client/src/routes/waiter/waiter.jsx
+++ burger-queen-api-client/src/routes/waiter/waiter.jsx
@@ -1,13 +1,54 @@
 // // import { useState, propTypes } from 'react';
 // // import Burger from './components/Burger';
 import Navbar from "./navbar.jsx";
+import { getProducts } from "../../fetch.js";
+import { useState, useEffect } from "react";
 
-const Waiter= () => {
+const Waiter = () => {
+  const [products, setProducts] = useState([]);
+  const [token, setToken] = useState('');
 
-return (
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    setToken(storedToken);
+  }, []);
+
+  useEffect (() => {
+    if(token){
+      fetchProducts(token)
+    }
+  }, [token])
+
+  const fetchProducts = (token) => {
+    getProducts(token)
+      .then((response) => {
+        console.log(response)
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching products:', error);
+      });
+  };
+
+  return (
     <>
-    <Navbar/>
-  </>
-);
-}
+      <Navbar />
+      <div className="card-container">
+        {products.map((product) => (
+          <div key={product.id} className="card">
+            <h3>{product.name}</h3>
+            <p>Price: {product.price}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
 export default Waiter;
