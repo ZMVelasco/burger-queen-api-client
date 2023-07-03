96fa90a7a6275244292fe8d6303b162adccf4ef0
Belen Neira <belen.neira05@gmail.com>
2023-07-03 17:55:34 -0400

    correcting modal

--- burger-queen-api-client/src/routes/waiter/waiter.jsx
+++ burger-queen-api-client/src/routes/waiter/waiter.jsx
@@ -1,29 +1,40 @@
-import { useState} from 'react';
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ProductCard from "./menu.jsx";
-import Sidebar from "./sidebar.jsx";
-import WaiterTracker from "./wtracker.jsx";
-
+import Sidebar from "../../sidebar-reu.jsx";
+// import WaiterPendinger from "./wPendinger.jsx";
+import KitchenOrders from "../chef/kitchenorders.jsx";
 
 const Waiter = () => {
   const [showCreateOrder, setShowCreateOrder] = useState(false);
-  const [showTrackOrder, setShowTrackOrder] = useState(false);
+  const [showPendingOrder, setShowPendingOrder] = useState(false);
 
   const handleCreateOrderClick = () => {
     setShowCreateOrder(true);
-    setShowTrackOrder(false);
+    setShowPendingOrder(false);
   };
 
-  const handleTrackOrderClick = () => {
+  const handlePendingOrderClick = () => {
     setShowCreateOrder(false);
-    setShowTrackOrder(true);
+    setShowPendingOrder(true);
   };
 
   return (
     <>
-      <Sidebar onCreateOrderClick={handleCreateOrderClick} onTrackOrderClick={handleTrackOrderClick} />
-      {showCreateOrder && <ProductCard />}
-      {showTrackOrder && <WaiterTracker />}
+      <Sidebar
+        items={[
+          { label: "Product Card", component: "product-card" },
+          { label: "Pending orders", component: "pending-orders" },
+        ]}
+        brandName="BQ"
+        welcomeMessage="Welcome, Waiter!"
+        onCreateOrderClick={handleCreateOrderClick}
+        onPendingOrderClick={handlePendingOrderClick}
+      />
+         <div className="content">
+        {showCreateOrder && <ProductCard />}
+        {showPendingOrder && <KitchenOrders />}
+      </div>
     </>
   );
 };
---
+++ burger-queen-api-client/src/sidebar-reu.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import logoutIcon from "./assets/logout.svg";
+import ProductCard from "./routes/waiter/menu";
+import WaiterTracker from "./routes/waiter/wtracker";
+import KitchenOrders from "./routes/chef/kitchenorders";
+
+const Sidebar = ({
+  items,
+  brandName,
+  welcomeMessage,
+  onCreateOrderClick,
+  onPendingOrderClick,
+  onPastOrderClick
+}) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const [componentToRender, setComponentToRender] = useState(null);
+
+  const navigate = useNavigate();
+
+  const toggleOffcanvas = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLogout = () => {
+    navigate("/");
+  };
+
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-dark fixed-top">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasDarkNavbar"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className={`offcanvas offcanvas-start text-bg-dark ${
+              isOpen ? "show" : ""
+            }`}
+            tabIndex="-1"
+            id="offcanvasDarkNavbar"
+            aria-labelledby="offcanvasDarkNavbarLabel"
+          >
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                {brandName}
+              </h5>
+              <button
+                type="button"
+                className="btn-close btn-close-white"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+                onClick={toggleOffcanvas}
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav">
+                {items.map((item, index) => (
+                  <li className="nav-item" key={index}>
+                    <a
+                      className="nav-link"
+                      onClick={() => {
+                        if (item.component === "product-card") {
+                          onCreateOrderClick();
+                        } else if (item.component === "pending-orders") {
+                          onPendingOrderClick();
+                        } else if (item.component === "past-orders") {
+                          onPastOrderClick();
+                        }
+                      }}
+                      data-component={item.component}
+                    >
+                      {item.label}
+                    </a>
+                  </li>
+                ))}
+                <li className="nav-item">
+                  <button
+                    className="btn btn-success"
+                    type="button"
+                    id="sidebar-logout"
+                    onClick={handleLogout}
+                  >
+                    <img
+                      src={logoutIcon}
+                      alt="Logout icon"
+                      className="logout-icon"
+                    />
+                    Log out
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <h5 className="navbar-brand">{welcomeMessage}</h5>
+        </div>
+      </nav>
+      {componentToRender && componentToRender === "product-card" && (
+        <ProductCard />
+      )}
+      {componentToRender && componentToRender === "pending-orders" && (
+        <KitchenOrders />
+      )}
+    </>
+  );
+};
+
+export default Sidebar;
+
+
